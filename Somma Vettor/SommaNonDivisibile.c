676105b67168aa3b46c1782cff5d6b32a6a5afc1
Ssdj3nt <92294163+Ssdj3nt@users.noreply.github.com>
2024-08-13 11:17:21 +0200

    Update SommaNonDivisibile.c

--- Somma Vettor/SommaNonDivisibile.c
+++ Somma Vettor/SommaNonDivisibile.c
@@ -1,97 +1,69 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <omp.h>
-#include <time.h>
+#include <stdlib.h>
 
-//Somma 2 vettori caso dove N non è divisibile per t
+void visualizzaArray(int *array, int size) {
+    for (int i=0; i<size;i++) {
+        printf("%d ",array[i]);
+    }
+}
 
-void  stampaVettore(float*, int, char[]);
+int main() {
+    int N;
 
-int main() 
-{
-    int i, N, t, N_Loc, indice;
+    printf("\nDefinisci N: ");
+    scanf("%d",&N);
 
-    int r, id, step; //Variabili aggiunte rispetto è quando è divisibile
+    int *a = calloc(N,sizeof(int));
+    int *b = calloc(N,sizeof(int));
+    int *c = calloc(N,sizeof(int));
 
-    float *a, *b, *c;
-
-    srand( time( NULL ) );
-
-
-
-    printf("Inserisci N \n");
-    scanf("%d", &N);
-
-    //Allocazione
-    a = (float *)calloc(N, sizeof(float));
-    b = (float *)calloc(N, sizeof(float));
-    c = (float *)calloc(N, sizeof(float));
-
-    //Valore vettori A e B
-    for(i = 0; i< N; i++)
-    {
-        a[i] = rand() %100 + 1;
+    for(int i=0; i<N; i++) {
+        a[i] = i;
+        b[i] = i + 1;
     }
 
-    
-    for(i = 0; i< N; i++)
+    printf("\nArray A:\n");
+    visualizzaArray(a,N);
+    printf("\n");
+    printf("\nArray B:\n");
+    visualizzaArray(b,N);
+    printf("\n");
+
+#pragma omp parallel shared (a, b, c)
     {
-        b[i] = rand() %100 + 1;
-    }
+        int t = omp_get_num_threads();
+        int id = omp_get_thread_num();
+        int N_Loc = N/t;
+        int step;
+        int indice;
 
-    //Stampa dei vettori
-    stampaVettore(a, N, "Vettore A:");
-    stampaVettore(b, N, "Vettore B:");
-
-
-    #pragma omp parallel private (N_Loc, i, indice, id, step) shared (a, b, c, r)
-    {
-        t = omp_get_num_threads();
-        id = omp_get_thread_num();
-        N_Loc = N/t;
-
-        r= N % t; //Calcolo resto della divisione
-
-        //printf("sono %d, di %d: numeri %d, r=%d\n",id,t,N_Loc,r);
+        int r= N % t; //Calcolo resto della divisione
 
         if(id < r) //Se il core ha un identificativo minore del resto si deve occupare di calcolare un elemento in più del vettore soluzione C
         {
             N_Loc++;
-            step=0;  
+            step=0;
         } else
         {
             step = r;
         } //Step serve ad ogni core di sapere di quali elementi di A e B deve occuparsi
 
-        printf("sono %d, di %d: numeri %d, r=%d, passo=%d\n",id,t,N_Loc,r,step);
-        
-        for(i=0; i < N_Loc; i++)
+        for(int i=0; i < N_Loc; i++)
         {
             indice = i + N_Loc * omp_get_thread_num() + step;
 
             c[indice] = a[indice] + b[indice];
         }
-    } //Fine direttiva 
+    }
 
-    stampaVettore( c, N, "Vettore C:" );
-
+    printf("\nArray c:\n");
+    visualizzaArray(c,N);
     printf("\n");
 
+    free(a);
+    free(b);
+    free(c);
+
     return 0;
-
-
-}
-
-
-
-//Funzione per la stampa del vettore
-void stampaVettore( float* a, int N, char name[] ) {
-
-    printf( "\n%s:\n", name );
-
-    for ( int i = 0; i < N; i++ )
-    {        
-        printf( "%f ", a[i] );
-        printf( "\n" );
-    }
 }
