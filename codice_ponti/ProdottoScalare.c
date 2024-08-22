11549e1740a1ae0e40b0d85e8de73ab15fad3ad0
Ssdj3nt <92294163+Ssdj3nt@users.noreply.github.com>
2024-08-22 11:41:07 +0200

    Update ProdottoScalare.c

--- codice_ponti/ProdottoScalare.c
+++ codice_ponti/ProdottoScalare.c
@@ -1,63 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
 #include <time.h>
+#include <omp.h>
 
-//Prodotto Scalare di due Vettori A e B in una variabile
-
-void  stampaVettore(float*, int, char[]);
-
-int main(){
-
-    int i, N;
-    float *a ,*b, result;
-
-    srand( time( NULL ) );
-
-    printf( "\nInserire N: " );
-    scanf( "%d", &N );
-    
-    // allocazione
-    a = ( float* ) calloc( N, sizeof( float ) );
-    b = ( float* ) calloc( N, sizeof( float ) );
-
-    //riempimento Vettori A e B
-    for(i = 0; i < N; i++)
-    {
-        a[i] = rand() % 100 + 1;
+void popolaArray(int *array, int size) {
+    for(int i = 0; i < size; i++) {
+        array[i] = rand() % 100 + 1;
     }
-    
-    for(i = 0; i < N; i++)
-    {
-        b[i] = rand() % 100 + 1;
+}
+void visualizzaArray(int *array, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ",array[i]);
     }
+}
 
-    stampaVettore(a, N, "Vettore A:");
-    stampaVettore(b, N, "Vettore B:");
+int main(void) {
 
-    #pragma omp parallel for private(i) shared(a,b) reduction(+:result)
-        for(i = 0; i < N; i++)
-        {
+    int N,result=0;
+
+    printf("\nDefinisci la grandezza dei vettori a e b: ");
+    scanf("%d",&N);
+
+    int *a = calloc(N,sizeof(int));
+    int *b = calloc(N,sizeof(int));
+
+    srand(time(NULL));
+    popolaArray(a,N);
+    popolaArray(b,N);
+
+    printf("\nA: ");
+    visualizzaArray(a,N);
+    printf("\nB: ");
+    visualizzaArray(b,N);
+
+#pragma omp parallel for shared (a,b) reduction(+:result) //Si parallelizza il ciclo for.
+        for(int i = 0; i < N; i++) {
             result += a[i]*b[i];
         }
 
-    printf("\n Risultato: %f\n", result);
+    printf("\n\nIl risultato derivato dal prodotto scalare di a e b: %d\n",result);
 
     free(a);
     free(b);
-
     return 0;
-}
 
-
-//Funzione per la stampa del vettore
-void stampaVettore( float* a, int N, char name[] ) {
-
-    printf( "\n%s:\n", name );
-
-    for ( int i = 0; i < N; i++ )
-    {        
-        printf( "%f ", a[i] );
-        printf( "\n" );
-    }
 }
