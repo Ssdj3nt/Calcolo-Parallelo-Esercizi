9b86c2bff8ac1bc2136f3e5601348e4991e50369
Ssdj3nt <92294163+Ssdj3nt@users.noreply.github.com>
2024-08-30 17:24:31 +0200

    Update Matrice_X_Vettore_PrimaStrategia.c

--- Prodotto Matrice Vettore/Matrice_X_Vettore_PrimaStrategia.c
+++ Prodotto Matrice Vettore/Matrice_X_Vettore_PrimaStrategia.c
@@ -1,119 +1,84 @@
-
-#include <stdlib.h>
 #include <stdio.h>
-#include <omp.h>
+#include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
-//Prodotto matrice vettore prima strategia
-
-#define N 16
-#define M 8
-#define VECTORSIZE M  //PRIMA STRATEGIA
-#define RESULTVECTORSIZE N //PRIMA STRATEGIA
-#define MAXVALUE 9
-
-void AllocazioneMatriceVettore( int***, int**, int** );
-void RiempiMatriceVettore( int***, int**, int** );
-void StampaMatrice( int**, char[] );
-void StampaVettore( int*, int, char[] );
-void DeallocaMatriceVettore( int***, int**, int** );
-
-int main()
-{
-
-    int** matrice;
-    int* vettore;
-    int* risultato;
-    int i,j;
-
-    srand(time(NULL));
-
-    AllocazioneMatriceVettore(&matrice, &vettore, &risultato);
-    RiempiMatriceVettore( &matrice, & vettore, &risultato);
-
-
-    StampaMatrice(matrice, "Matrice Iniziale:");
-    StampaVettore(vettore, VECTORSIZE, "Vettore Iniziale");
-
-    #pragma omp parallel for default(none) shared (matrice, vettore, risultato) private(i,j)
-    for( i= 0; i<N; i++)
-    {
-        for (j=0;j<M; j++)
-        {
-            risultato[i] += matrice[i][j] * vettore[j];
+void fillMatrix(int **Matrix, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            Matrix[i][j] = i + j;
         }
     }
+}
 
-    StampaVettore(risultato, RESULTVECTORSIZE, "Vettore risultato:");
-    DeallocaMatriceVettore( &matrice, &vettore, &risultato);
+void fillVector(int *Vector, int m) {
+    for (int i = 0; i < m; i++) {
+        Vector[i] = i;
+    }
+}
 
+void printMatrix(int **Matrix, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", Matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void printVector(int *Vector, int m) {
+    for (int i = 0; i < m; i++) {
+        printf("%d ", Vector[i]);
+    }
+    printf("\n");
+}
+
+int main(void) {
+    int n, m;
+   
+    printf("\nDefinisci le righe della matrice: ");
+    scanf("%d", &n);
+    printf("\nDefinisci le colonne della matrice: ");
+    scanf("%d", &m);
+   
+    // Alloco la matrice
+    int **Matrix = calloc(n, sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        Matrix[i] = (int*)calloc(m, sizeof(int));
+    }
+   
+    // Alloco i vettori
+    int *Vector = calloc(m, sizeof(int));
+    int *Result = calloc(n, sizeof(int)); // Corretto, ora ha dimensione n
+   
+    fillMatrix(Matrix, n, m);
+    fillVector(Vector, m);
+   
+    printf("\nMatrice:\n");
+    printMatrix(Matrix, n, m);
+   
+    printf("\nVettore:\n");
+    printVector(Vector, m);
+   
+    // Calcolo del prodotto matrice-vettore in parallelo
+    #pragma omp parallel for shared(Matrix, Vector) reduction(+:Result[:n])
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            Result[i] += Matrix[i][j] * Vector[j];
+        }
+    }
+   
+    printf("\nRisultato del prodotto matrice-vettore:\n");
+    printVector(Result, n);
+    printf("\n");
+   
+    // Liberazione della memoria
+    for (int i = 0; i < n; i++) {
+        free(Matrix[i]);
+    }
+    free(Matrix);
+    free(Vector);
+    free(Result);
+   
     return 0;
-}
-
-
-void AllocazioneMatriceVettore( int*** matrix, int** vector, int** result ) {
-    // Allocazione della memoria per le righe della matrice
-     *matrix = ( int** ) calloc( N, sizeof( int* ) );
-
-    // Allocazione della memoria per ogni riga della matrice
-    for ( int i = 0; i < N; i++ ){
-        ( *matrix )[i] = ( int* ) calloc( M, sizeof( int ) );
-
-    }
-    // Allocazione della memoria per il vettore iniziale
-    *vector = ( int* ) calloc( VECTORSIZE, sizeof( int ) );
-    
-    // Allocazione della memoria per il vettore risultato
-    *result = ( int* ) calloc( RESULTVECTORSIZE, sizeof( int ) );
-}
-
-//Riempimento della matrice, del vettore iniziale e vettore risultato
-void RiempiMatriceVettore( int*** matrix, int** vector, int** result ) {
-    //Matrice
-    for ( int i = 0; i < N; i++ )
-    {
-        for ( int j = 0; j < M; j++ )
-        {
-            ( *matrix )[i][j] = rand() % MAXVALUE + 1;
-        }
-    }
-    //Vettore iniziale
-    for ( int i = 0; i < VECTORSIZE; i++ )
-    {
-        ( *vector )[i] = rand() % MAXVALUE + 1; 
-    }
-    //Vettore risultato
-    for ( int i = 0; i < RESULTVECTORSIZE; i++ )
-    {
-        ( *result )[i] = 0;
-    }
-}
-
-
-//Stampa della matrice
-void StampaMatrice( int** matrix, char name[] ) {
-    printf( "\n%s:\n", name );
-    for ( int i = 0; i < N; i++ ) {
-        for ( int j = 0; j < M; j++ )
-            printf( "%d    ", matrix[i][j] );
-        printf( "\n" );
-    }
-}
-
-//Stampa vettore
-void StampaVettore( int* vector, int n, char name[] ) {
-    printf( "\n%s: ", name );
-    for ( int i = 0; i < n; i++ )
-        printf( "%d ", vector[i] );
-    printf( "\n" );
-}
-
-
-//Deallocazione in memoria della matrice, vettore iniziale e vettore risultato
-void DeallocaMatriceVettore( int*** matrix, int** vector, int** result ) {
-    for ( int i = 0; i < N; i++ )
-        free( ( *matrix )[i] );
-    free( *matrix );
-    free( *vector );
-    free( *result );
 }
